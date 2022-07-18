eae931edbc78e963ca2bba9adaf6300f2f252b77
lawandepranav <lawandepranav@gmail.com>
2022-07-18 21:17:16 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components"
+import  {Link} from "react-router-dom"
+import Home from "../pages/Home"
+
 
 const Navbar = () => {
-  return <div>{/* CODE HERE */}</div>;
+
+  return (
+<>
+<h1>Lawande</h1>
+</>
+    // <NavbarWrapper>
+    //    <StyledLink to = "/">Home</ StyledLink>
+    //    <StyledLink to = "/login">Login </StyledLink>
+    // </NavbarWrapper>
+  )
 };
 
 export default Navbar;
